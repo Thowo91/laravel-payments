352f24647a42ad8a46d9563fd469438889d021ad
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2019-12-12 10:35:52 +0100

    Paypal Access Token

--- config/paypal.php
+++ config/paypal.php
@@ -6,7 +6,8 @@ return [
     'credentials' => [
         'sandbox' => [
             'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
-            'secret' => env('PAYPAL_SANDBOX_SECRET', '')
+            'secret' => env('PAYPAL_SANDBOX_SECRET', ''),
+            'access_token' => env('PAYPAL_SANDBOX_ACCESS_TOKEN'),
         ],
     ],
     /**
